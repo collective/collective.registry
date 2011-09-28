31bb3963e0abbc7e1bdf1054d8a84397bbed9883
JeanMichel FRANCOIS <toutpt@gmail.com>
2011-09-28 16:45:19 +0200

    move to rst

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0'
 setup(name='collective.registry',
       version=version,
       description="Extend plone.app.registry by implementing IRegistry with an adapter on content",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
